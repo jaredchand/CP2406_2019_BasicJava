c154805df50a2467fb857ff9c657d67b54749514
jared <jared.chand@my.jcu.ed.au>
2019-08-21 08:49:43 +1000

    Added week2 & week3 Package to src

--- src/week2/DogTest.java
+++ src/week2/DogTest.java
@@ -1,3 +1,5 @@
+package week2;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +10,8 @@ class DogTest {
         Dog dog = new Dog();
         assertNull(dog.name);
         assertEquals(0, dog.age);
-        assertEquals(null, dog.breed);
-        assertEquals(null, dog.shotStatus);
+        assertNull(dog.breed);
+        assertNull(dog.shotStatus);
     }
 
 }
