5be2fd20312b0830ada14a919f93bba5c28304b0
jared <jared.chand@my.jcu.ed.au>
2019-08-21 09:37:09 +1000

    New InchesToFeet class/test to calculate inches to feet conversion

---
+++ src/week3/InchesToFeet.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class InchesToFeet {
+    final int INCHES = 86;
+    double feet = INCHES * 0.08333;
+
+    void printValues() {
+        System.out.println(INCHES + " Inches " + "is " + feet + " Feet ");
+    }
+}
---
+++ src/week3/InchesToFeetTest.java
@@ -0,0 +1,12 @@
+package week3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InchesToFeetTest {
+    @Test void printValues() {
+        InchesToFeet inchestofeet = new InchesToFeet();
+        inchestofeet.printValues();
+    }
+}
