147ed9b9ff0bada181ed85de613d14ae498f914d
jared <jared.chand@my.jcu.ed.au>
2019-09-03 13:13:50 +1000

    DebugFive2 file successfully debugged.

--- src/week5/DebugFive2.java
+++ src/week5/DebugFive2.java
@@ -1,25 +1,24 @@
 // DebugFive2.java
 // Decides if two numbers are evenly divisible
 package week5;
+
 import java.util.Scanner;
-public class DebugFive2 {
-   public static void main(String args[]) {
-      int num;
-      int num2;
 
-      Scanner input = new Scanner(System.in);
+public class DebugFive2
+{
+    public static void main(String[] args)
+    {
+        int num;
+        int num2;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+        System.out.print("Enter another number: ");
+        num2 = input.nextInt();
+        if ((num%num2 == 0) && (num2%num == 0))
+            System.out.println("One of these numbers is evenly divisible into the other");
+        else
+            System.out.println("Neither of these numbers is evenly divisible into the other");
+    }
 
-      System.out.print("Enter a number ");
-      num = Integer.parseInt(input.next());
-
-      System.out.print("Enter another number ");
-      num2 = Integer.parseInt(input.next());
-
-      if ( (num2 / num) = 0 && (num / num2 = 0)) {
-         System.out.println("One of these numbers is evenly divisible into the other");
-      } else {
-         System.out.println("Neither of these numbers is evenly divisible into the other");
-      }
-
-   }
 }
