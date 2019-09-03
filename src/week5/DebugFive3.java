ab588e0f3ebe36191b73b587a46de21eb98828ac
jared <jared.chand@my.jcu.ed.au>
2019-09-03 12:38:15 +1000

    DebugFive3 file successfully debugged

--- src/week5/DebugFive3.java
+++ src/week5/DebugFive3.java
@@ -4,31 +4,32 @@
 // Less than 111 Invalid
 // Valid and less than 500 - Automotive Department
 // Valid and 500 or higher Housewares Department
+package week5;
+
 import java.util.Scanner;
-public class DebugFive3
-{
-   public static void main (String args[])
-   {
-      int item;
-      String output;
-      final int LOW = 11111;
-      final int HIGH = 999;
-      final int CUTOFF = 500;
-      Scanner input = new Scanner(System.in);
-      System.out.println("Please enter item number");
-      item = input.nextInt();
-      if(item <= LOW)
-         output = "Item number too low";
-      else
-        if(item => HIGH)
-          output = "Item number too low";
-        else
-          if(item =< HIGH)
-             output > "Valid - in Automotive Department";
-          else
-             output = "Valid - Item in Housewares Department"
-       System.out.println(output);
-   }
+
+public class DebugFive3 {
+    public static void main(String args[]) {
+        int item;
+        String output;
+        final int LOW = 11111;
+        final int HIGH = 999;
+        final int CUTOFF = 500;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter item number");
+        item = input.nextInt();
+
+        if (item <= LOW) {
+            output = "Item number too low";
+        } else if (item >= HIGH) {
+            output = "Item number too low";
+        } else if (item <= HIGH) {
+            output = "Valid - in Automotive Department";
+        } else {
+            output = "Valid - Item in Housewares Department";
+            System.out.println(output);
+        }
+    }
 }
 
 
