01dae1d2a181d02977f16ffa93780d2634898b31
jaredchand <jared.chand@my.jcu.edu.au>
2019-09-25 09:16:21 +1000

    Chapter 8 - CareCareChoice & CarCareChoice2 Classes

--- src/week6/CarCareChoice2.java
+++ src/week6/CarCareChoice2.java
@@ -2,18 +2,20 @@ package week6;
 
 import java.util.Scanner;
 
-public class CarCareChoice {
+public class CarCareChoice2 {
     public static void main(String[] args) {
         String[] stringArray = {"oil change", "tyre rotation", "battery check", "brake inspection"};
         int[] intArray = {25, 22, 15, 5};
         int correctIndex = -1;
-
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a required service from the choices below: \n" + stringArray[0] + "\n" + stringArray[1] + "\n"+ stringArray[2] + "\n"+ stringArray[3] + "\n" + ">>>");
+        System.out.println("Please enter first 3 letters of a required service from the choices below: \n" + stringArray[0] + "\n" + stringArray[1] + "\n"+ stringArray[2] + "\n"+ stringArray[3] + "\n");
         String userInput = input.nextLine();
-
+        String userSubstring;
+        String substring;
         for (int i=0; i<stringArray.length; ++i){
-            if (userInput.equals(stringArray[i])){
+            substring = stringArray[i].substring(0,3);
+            userSubstring = userInput.substring(0,3);
+            if (userSubstring.equals(substring)){
                 correctIndex = i;
             }
         }
